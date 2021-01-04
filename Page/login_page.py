248654bfede50a4fff22c3028e3c8b7d99003976
monica <563455843@qq.com>
2021-01-04 17:55:43 +0800

    增加ddt获取方法

--- Page/login_page.py
+++ Page/login_page.py
@@ -1,27 +1,30 @@
+import os
 import time
+
+import yaml
 
 from public.base import driver
 from public.opertion_element import Element
+PATH = os.path.dirname(os.path.dirname(__file__))
+data_load = yaml.load(open(PATH + '//Yaml//page_element.yaml','r',encoding='utf-8'))
+login_data = data_load.get('login')
 
-username_password = 'el-input__inner'
-click_logon = 'el-button,login-btn,el-button--primary'
+
+
 class login_action(Element):
 
-    def log_on(self,*args):
-
-        user_mess = self.find_elements_class(username_password)
+    def log_on(self,**kwargs):
+        user_mess = self.find_elements_class(login_data.get('username_password'))
         i = 0
+        user_mess[i].send_keys(kwargs.get('username'))
+        user_mess[i+1].send_keys(kwargs.get('password'))
+
+        self.find_element_class(login_data.get('click_logon')).click()
+
+        #self.get_alter_text()
+if __name__ == '__main__':
 
 
-        user_mess[i].send_keys(args[0])
-
-        user_mess[i+1].send_keys(args[1])
-
-
-        self.find_element_class(click_logon).click()
-
-        self.get_alter_text()
-
-
-ll = login_action(driver(),'http://39.101.216.196/sdr/#/login')
-ll.log_on('lijie7','12345678')
+    ll = login_action(driver())
+    # 'username'='lijie7','password'='123456789'
+    ll.log_on(username='lijie7',password='12345')
---
+++ run.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+
+
+import ddt
+
+from Page.login_page import login_action
+from public.base import driver
+PATH = os.path.dirname(__file__)
+test_data_path = PATH + '//YAML//' + 'test_data.yaml'
+
+@ddt.ddt
+class run_main(unittest.TestCase):
+    def setUp(self) -> None:
+        self.logon = login_action(driver())
+    @ddt.file_data(test_data_path)
+
+    def test_login_001(self,**kwargs):
+
+        self.logon.log_on(**kwargs)
+
+
+if __name__ == '__main__':
+    unittest.main()
