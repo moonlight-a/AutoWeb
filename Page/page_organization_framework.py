d7ac84f371f57e4b7cb5987e575aa49c6c65b62f
monica <563455843@qq.com>
2021-02-04 17:31:12 +0800

    优化代码

--- Page/page_organization_framework.py
+++ Page/page_organization_framework.py
@@ -1,53 +1,67 @@
-import os,random,time,yaml
-
-import allure
-
+import os,time,yaml, allure
 from Page.login_page import login_action
 from public.base import driver
 from Tools.sql_tools import connect_sql
 from selenium.webdriver.common.by import By
-
-from public.operation_public_page import operation_page_function
-
 PATH = os.path.dirname(os.path.dirname(__file__))
+organ_framework = yaml.load(open(PATH + '//page_elements//page_organization_element.yaml','r',encoding='utf-8'))
 
-
-page_public_element = yaml.load(open(PATH + '//page_elements//page_public_element.yaml','r',encoding='utf-8'))
-connect_sql = connect_sql()
-'''查询条件元素信息'''
-
-
-Select_add_organization_element = (By.XPATH,page_public_element.get('Select_add_organization_element'))
-Select_add_value_elenment = (By.XPATH,page_public_element.get('Select_add_value_elenment'))
-Select_add_role_element = (By.XPATH,page_public_element.get('Select_add_role_element'))
-Add_Login_id_element = (By.XPATH,page_public_element.get('Add_Login_id_element'))
-Add_name_element = (By.XPATH,page_public_element.get('Add_name_element'))
-Add_Job_number_element = (By.XPATH,page_public_element.get('Add_Job_number_element'))
-Add_Phone_element = (By.XPATH,page_public_element.get('Add_Phone_element'))
-Add_Position_element = (By.XPATH,page_public_element.get('Add_Position_element'))
-Add_Email_element = (By.XPATH,page_public_element.get('Add_Email_element'))
-
-wwwwwwww = (By.XPATH,page_public_element.get('organization_test_element'))
-
+#添加组织
+ADD_ORGANIATION_BUTTON = (By.XPATH,organ_framework.get('Add_organization_element'))
+#组织名称
+ORGANIATION_NAME = (By.XPATH,organ_framework.get('Add_organization_name_element'))
+#组织编号
+ORGANIATION_ID = (By.XPATH,organ_framework.get('Add_organization_id_element'))
+#组织确定提交按钮
+ADD_ORGANIATION_CONFIRM_BUTTON = (By.XPATH,organ_framework.get('Add_organization_confirm_element'))
 
 class page_one(login_action):
-
-
-
 
     @allure.step('进入系统首页')
     @allure.description("点击页面系统管理模块，进入组织架构")
     def enter_organization(self):
         self.login_on(username='monica', password='123456')
         self.enter_moudle_name(0)
-        self.operation_child_element()
+        time.sleep(1)
+        self.operation_child_element(0)
+
+    @allure.step('点击高级查询按钮')
+    @allure.description("点击高级查询按钮，展开全部查询条件")
+    def click_senior_button(self):
+        self.operation_senior_button()
+    @allure.step('输入查询条件值')
+    @allure.description("操作查询按钮，输入查询条件值")
+    def input_search_value(self,index,**kwargs):
+        #操作文本输入框
+        self.opeation_input_text_query(**kwargs)
+        #操作需输入的下拉框
+        self.opeation_select_input_query(index)
+        #操作有默认值的下拉框
+        self.operation_select_default_query(index)
+        #点击查询按钮
+        self.operation_search_button()
+
+    @allure.step('点击新增按钮（树菜单下拉框封装无法选中值')
+    @allure.description("打开新增窗口，新增值")
+    def Add_user_data(self):
+        pass
+
+    @allure.step('点击添加组织按钮')
+    @allure.description("添加组织数据")
+    def Add_organization_data(self,*args):
+        self.find_element_by_xpath(ADD_ORGANIATION_BUTTON).click()
+        self.find_element_by_xpath(ORGANIATION_NAME).send_keys(args[0])
+
+        self.find_element_by_xpath(ORGANIATION_ID).send_keys(args[1])
+        time.sleep(1)
+        self.operation_confirm_button()
 
 
-
-
-
-
-
+    def main(self):
+        self.enter_organization()
+        self.Add_organization_data('自动化测试','1234')
+        # self.click_senior_button()
+        # self.input_search_value(1,search_one='1234',search_two='987654',search_three='15155972770')
 
 
 if __name__ == '__main__':
@@ -57,7 +71,7 @@ if __name__ == '__main__':
     # 'username'='lijie7','password'='123456789'
 
 
-    ll.enter_organization()
+    ll.main()
 
 
 
